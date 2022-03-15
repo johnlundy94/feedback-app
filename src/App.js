5e104b9ff74bdeaf80efd17a716f841a4502307d
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-03-15 09:24:36 -0600

    Adding state to the feedback items.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeebackItem";
+import FeedbackItem from "./components/FeedbackItem";
 
 function App() {
   return (
