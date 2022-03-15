cfa4022a24106f6e217580d99433e0ba6fc91ab6
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-03-15 09:06:06 -0600

    Added Feedback Items.

---
+++ src/components/FeebackItem.jsx
@@ -0,0 +1,10 @@
+function FeebackItem() {
+  return (
+    <div className="card">
+      <div className="num-display">10</div>
+      <div className="text-display">This is an example</div>
+    </div>
+  );
+}
+
+export default FeebackItem;
