1ebc5ef889df363daf6d98614558ca2aa30752aa
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-03-15 08:59:36 -0600

    Updated header style.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import PropTypes from "prop-types";
 
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
   return (
-    <header>
+    <header style={headerStyles}>
       <div className="container">
         <h2>{text}</h2>
       </div>
@@ -12,6 +16,8 @@ function Header({ text }) {
 
 Header.defaultProps = {
   text: "Feedback App",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
 };
 
 Header.propTypes = {
