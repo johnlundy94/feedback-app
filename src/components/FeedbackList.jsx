879418d9797d3c450c004fe33336bd95f0ce1d9d
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-03-16 08:39:07 -0600

    FeedbackList is being rendered

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,5 @@
+import FeedbackItem from "./FeedbackItem";
+
 function FeedbackList({ feedback }) {
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet.</p>;
@@ -5,7 +7,7 @@ function FeedbackList({ feedback }) {
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <div>{item.rating}</div>
+        <FeedbackItem key={item.id} item={item} />
       ))}
     </div>
   );
