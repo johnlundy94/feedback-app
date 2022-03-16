855becad37a68c339cc16ea472244670751be008
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-03-16 08:23:35 -0600

    Added feedback data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackItem from "./components/FeedbackItem";
+import FeedbackData from "./data/feedbackData";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
   return (
     <>
       <Header />
