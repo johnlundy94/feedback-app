bb6a18904cfff2c7cb91cb773d4174959293d48f
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-03-18 09:24:31 -0600

    fixed button

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
+
 function FeedbackForm() {
-  return <div>FeedbackForm</div>;
+  const [text, setText] = useState("");
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate us?</h2>
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" version="secondary">
+            Send
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
 }
 
 export default FeedbackForm;
