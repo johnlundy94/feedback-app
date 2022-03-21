18b3a43453dba2ceaa541813c842ab71e7d55340
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-03-21 09:24:47 -0600

    Added About icon

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,15 +3,17 @@ import Card from "../components/shared/Card";
 function AboutPage() {
   return (
     <Card>
-      <h1>About This Project</h1>
-      <p>
-        This is a React app to leave feedback. Thank you Brad Traversy for
-        giving me this beautiful guide!
-      </p>
-      <p>Version: 1.0.0</p>
-      <p>
-        <a href="/">Back To Home</a>
-      </p>
+      <div className="about">
+        <h1>About This Project</h1>
+        <p>
+          This is a React app to leave feedback. Thank you Brad Traversy for
+          giving me this beautiful guide!
+        </p>
+        <p>Version: 1.0.0</p>
+        <p>
+          <a href="/">Back To Home</a>
+        </p>
+      </div>
     </Card>
   );
 }
