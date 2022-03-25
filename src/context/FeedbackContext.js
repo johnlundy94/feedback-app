026f43f811eb09581ae2a882c273f282fb326215
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-03-25 08:58:45 -0600

    Feedback provider useState was missing an empty array

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState();
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
